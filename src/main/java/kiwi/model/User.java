14cb7e0c3372aa1b5d767183d6542e0716f7ac66
Paweł Góralik <pawel.goralik@gmail.com>
2013-05-22 12:32:22 +0200

    Ehhh.. no i jeszcze pliki, w których wprowadziłem zmiany. W pom.xml są zmiany - zalecany UPDATE PROJEKTU MAVENOWSKIEGO.

--- src/main/java/kiwi/model/User.java
+++ src/main/java/kiwi/model/User.java
@@ -1,14 +1,38 @@
 package kiwi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class User {
-    protected String username;
-    protected String password;   
-    
-    public boolean authenticate() {
-        return true;
-    }
-    
-    public boolean authorizate() {
-        return true;
-    }
+	@Id
+	@GeneratedValue
+	private Long id;
+	protected String username;
+	protected String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
